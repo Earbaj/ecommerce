32fb5a7071755980aa95bd4ffe1d66bac129802d
Earbaj <earbajsaria3@gmail.com>
2026-03-12 12:12:48 +0600

    ADD:swager to beautiful ui api

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  // Swagger কনফিগারেশন
+  const config = new DocumentBuilder()
+    .setTitle('My API Title')
+    .setDescription('The API description')
+    .setVersion('1.0')
+    .addTag('users')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  
+  // Swagger UI সেটআপ (endpoint: /api)
+  SwaggerModule.setup('api', app, document);
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
