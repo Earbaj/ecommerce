c7f12d4e4c587c4f62b70f072f7f7f27f9278162
Earbaj <earbajsaria3@gmail.com>
2026-03-13 14:35:45 +0600

    Update jwt strategy for userId and product controller and modules and service added new updated methodes for products creation

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -1,4 +1,30 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, Get, Body, UseGuards, Req, Param,UnauthorizedException } from '@nestjs/common';
+import { ProductsService } from './products.service';
+import { CreateProductDto } from './dto/create-product.dto';
+import { AuthGuard } from '@nestjs/passport';
 
 @Controller('products')
-export class ProductsController {}
+export class ProductsController {
+  constructor(private readonly productsService: ProductsService) {}
+
+  @Post()
+  @UseGuards(AuthGuard('jwt')) // প্রোডাক্ট অ্যাড করতে লগইন মাস্ট
+  create(@Body() createProductDto: CreateProductDto, @Req() req) {
+    console.log('Log from Controller - User Object:', req.user);
+
+  if (!req.user || !req.user.userId) {
+    throw new UnauthorizedException('User not found in request');
+  }
+    return this.productsService.create(createProductDto, req.user.userId);
+  }
+
+  @Get()
+  findAll() {
+    return this.productsService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.productsService.findOne(id);
+  }
+}
--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,4 +1,27 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { Product } from './schemas/product.schema';
+import { CreateProductDto } from './dto/create-product.dto';
 
 @Injectable()
-export class ProductsService {}
+export class ProductsService {
+  constructor(@InjectModel(Product.name) private productModel: Model<Product>) {}
+
+  async create(createProductDto: CreateProductDto, userId: string) {
+    const newProduct = new this.productModel({
+      ...createProductDto,
+      createdBy: userId,
+    });
+    return await newProduct.save(); 
+  }
+
+  async findAll() {
+    // .populate('category') দিলে ক্যাটাগরির সব ডিটেইলস চলে আসবে (যেমন রিলেশনাল ডাটাবেসে হয়)
+    return await this.productModel.find().populate('category', 'name slug').exec();
+  }
+
+  async findOne(id: string) {
+    return await this.productModel.findById(id).populate('category').exec();
+  }
+}
