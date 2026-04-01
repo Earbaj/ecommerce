c7190a0225c83c3ab72f66cdebf75c0925dac837
Earbaj <earbajsaria3@gmail.com>
2026-04-01 11:28:42 +0600

    ADD: mongodb module to orders module

--- src/orders/orders.module.ts
+++ src/orders/orders.module.ts
@@ -1,8 +1,19 @@
 import { Module } from '@nestjs/common';
 import { OrdersService } from './orders.service';
 import { OrdersController } from './orders.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Order, OrderSchema } from './schemas/order.schema';
+import { Cart, CartSchema } from '../cart/schemas/cart.schema';
+import { Product, ProductSchema } from '../products/schemas/product.schema';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([
+      { name: Order.name, schema: OrderSchema },
+      { name: Cart.name, schema: CartSchema },
+      { name: Product.name, schema: ProductSchema },
+    ]),
+  ],
   providers: [OrdersService],
   controllers: [OrdersController]
 })
